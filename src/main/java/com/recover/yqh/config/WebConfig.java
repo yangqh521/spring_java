665634486d406a70d7d4afc37b03d7eeebe287de
yangqh521 <yangqh521@qq.com>
2019-03-25 14:46:06 +0800

    替换为fastjson

--- src/main/java/com/recover/yqh/config/WebConfig.java
+++ src/main/java/com/recover/yqh/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.recover.yqh.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -11,6 +14,10 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 @Configuration
 @EnableWebMvc	//启动springMVC注解驱动 等价于xml 配置中的<mvc:annotation-driven/>
@@ -55,6 +62,26 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
         super.addViewControllers(registry);
+    }
+	
+	@Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        /*
+         * 1、需要先定义一个 convert 转换消息的对象;
+         * 2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据;
+         * 3、在convert中添加配置信息.
+         * 4、将convert添加到converters当中.
+         */
+        // 1、需要先定义一个 convert 转换消息的对象;
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        //2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据;
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat );
+        //3、在convert中添加配置信息.
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        //4、将convert添加到converters当中.
+        converters.add(fastConverter);
     }
 
 }
